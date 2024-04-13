27ac692b5d1d759539cd2b934393b54bb0790f27
vineetkumarbb12 <127923790+vineetkumarbb12@users.noreply.github.com>
2024-04-13 22:05:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
+def welcome():
     return 'Hello World'
+@app.route('/name')
+def name():
+    return 'Devops'
+@app.route('/ip')
+def ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
 if __name__ == '__main__':
   app.run()
